b970650a4071bf8b79cd24e3c1431f9db18a3fa8
Bernat <bernat.orellana@gmail.com>
2021-03-23 11:07:12 +0100

    Scene Manager functions develop

--- src/brickbreaker/scenes/SceneManager.java
+++ src/brickbreaker/scenes/SceneManager.java
@@ -10,5 +10,40 @@ package brickbreaker.scenes;
  * @author bernat
  */
 public class SceneManager {
+    private int id;
+    private String name;
+    private int[][] matrix;
+
+    public SceneManager(int id, String name, int[][] matrix) {
+        this.id = id;
+        this.name = name;
+        this.matrix = matrix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    
     
 }
