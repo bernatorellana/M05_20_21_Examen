1a2e890329e6e382f13267fa433da20b76ea8fa4
bernat <bernat.orellana@gmail.com>
2021-03-25 16:47:49 +0100

    Bernat was here

--- src/brickbreaker/Ball.java
+++ src/brickbreaker/Ball.java
@@ -44,7 +44,8 @@ public class Ball extends Parent {
     }
 
     public int getDiameter() {
-        return diameter;
+        //Bernat was here
+        return diameter+1;
     }
 
     public void changeSize(int newSize) {
