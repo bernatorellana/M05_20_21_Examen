5115879d7a26632a4147d641372dc8b53b321bbf
JoanPol <jserarols@milaifontanals.org>
2021-03-25 17:04:35 +0100

    diameter

--- src/brickbreaker/Ball.java
+++ src/brickbreaker/Ball.java
@@ -44,7 +44,7 @@ public class Ball extends Parent {
     }
 
     public int getDiameter() {
-        return diameter;
+        return diameter+1;
     }
 
     public void changeSize(int newSize) {
