7cc64edc1feb171d25f621175daba3d599a416ff
pol_o <poliva@milaifontanals.org>
2021-03-25 17:10:00 +0100

    Canvis_diameter

--- src/brickbreaker/Ball.java
+++ src/brickbreaker/Ball.java
@@ -44,7 +44,7 @@ public class Ball extends Parent {
     }
 
     public int getDiameter() {
-        return diameter;
+        return diameter; //canvi
     }
 
     public void changeSize(int newSize) {
